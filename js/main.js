2f065ee94c17c6eab093cd3e0fd0bf896e965a30
chrmux <jnmarollano@gmail.com>
2022-01-08 14:17:32 +0800

    Last updated

--- js/main.js
+++ js/main.js
@@ -1,32 +1,8 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
+const yearDisplay = document.querySelector('span');
+var today = new Date();
+var todayYear = today.getFullYear();
+yearDisplay.textContent = (todayYear+"  .:|:. Jerry N. Marollano .:|:. Philippines");
 
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
+const modDateDisplay = document.querySelector('footer p');
+let modDate = document.lastModified;
+modDateDisplay.textContent = ("Last Updated "+modDate);
