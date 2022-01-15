f79b717b2e78e6f4ed2dc790a8f374b22d427b7d
chrmux <jnmarollano@gmail.com>
2022-01-15 23:54:32 +0800

    added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const yearDisplay = document.querySelector('span');
+const yearDisplay = document.querySelector('footer > span');
 var today = new Date();
 var todayYear = today.getFullYear();
-yearDisplay.textContent = (todayYear+"  .:|:. Jerry N. Marollano .:|:. Philippines");
+yearDisplay.textContent = (todayYear +"  There are " + daycount + " days until " + targetEvent + "!");
 
-const modDateDisplay = document.querySelector('footer p');
+const modDateDisplay = document.querySelector('footer > p');
 let modDate = document.lastModified;
 modDateDisplay.textContent = ("Last Updated "+modDate);
