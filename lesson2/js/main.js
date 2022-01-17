9b4902e300da69984c1f35dfc689c281a15f8ceb
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2022-01-17 22:35:08 +0800

    Update main.js

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -1,8 +1,34 @@
-const yearDisplay = document.querySelector('footer > span');
-var today = new Date();
-var todayYear = today.getFullYear();
-yearDisplay.textContent = (todayYear +"  There are " + daycount + " days until " + targetEvent + "!");
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// using getElementById
+document.getElementById("currentdate").textContent = fulldate;
 
-const modDateDisplay = document.querySelector('footer > p');
-let modDate = document.lastModified;
-modDateDisplay.textContent = ("Last Modified "+modDate);
+const date2 = document.querySelector('#currentdate2');
+
