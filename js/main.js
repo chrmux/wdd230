775a7ae8a633636b2b49963c15a674b951c64318
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2022-01-18 12:46:45 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,8 @@
-const daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
-// ****************************
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
-I hope it works on yours.
-
-const yearDisplay = document.querySelector('footer > span');
+const yearDisplay = document.querySelector('span');
 var today = new Date();
 var todayYear = today.getFullYear();
-yearDisplay.textContent = (todayYear +"  There are " + daycount + " days until " + targetEvent + "!");
+yearDisplay.textContent = (todayYear+"  | Jerry N. Marollano | - Philippines");
 
-const modDateDisplay = document.querySelector('footer > p');
+const modDateDisplay = document.querySelector('footer #currentdate2');
 let modDate = document.lastModified;
-modDateDisplay.textContent = ("Last Modified "+modDate);
+modDateDisplay.textContent = (""+modDate);
