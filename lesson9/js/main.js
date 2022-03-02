a337e75c0e9521e1ed32d3a4940f6e1421961aa0
chrmux <jnmarollano@gmail.com>
2022-03-03 04:37:44 +0800

    Added

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -1,23 +1,5 @@
-
-//const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-//const myList = document.querySelector('ul');
-
-//fetch('requestURL')
-//  .then(function (response) {
-//    return response.json();
-//  })
-//  .then(function (json) {
-//    for (var i = 0; i < json.prophets.length; i++) {
-//      var listItem = document.createElement('li');
-//      listItem.innerHTML = json.prophets[i].name + ' ' + json.prophets[i].lastname + ' <br>';
-//     listItem.innerHTML += 'Place of Birth:' + json.prophets[i].birthplace + ' <br>';
-//     listItem.innerHTML += json.prophets[i].imageurl;
-//     myList.appendChild(listItem);
-//    }
-    
-//  });
-
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
 
 fetch(requestURL)
   .then(function (response) {
@@ -25,20 +7,42 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+    let prophets = jsonObject['prophets'];
+      prophets.forEach(displayProphets);
   });
-  
-const prophets = jsonObject('phrophets');
-prophets.forEach(displayProphets);
 
-function displayProphets(prophet) {  // Create elements to add to the document
+function displayProphets(prophet) {
+  // Create elements to add to the document
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
-    // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+  let p = document.createElement('p');
+  let portrait = document.createElement('img');
+  
+
+
+  // Change the textContent property of the h2 element to contain the prophet's full name
+  h2.textContent = prophet.name + ' ' + prophet.lastname;
+  p.textContent += 'Date of Birth: ' + prophet.birthdate, ' <br>';
+  p.textContent += 'Birth Place: ' + prophet.birthplace;
+  
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  portrait.setAttribute('src', prophet.imageurl);
+  portrait.setAttribute('loading', 'lazy');
+  portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname + '-' +prophet.order + 'th Latter-day President');
+
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
+  card.appendChild(p);
+  card.appendChild(portrait);
+
   // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('div.cards').appendChild(card);
+  cards.appendChild(card);
 }
+
+
+
+
+
 
 
