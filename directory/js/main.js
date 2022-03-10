f9adbd12157dee3254af30607fca27581cc7ff03
chrmux <jnmarollano@gmail.com>
2022-03-10 22:58:38 +0800

    Added

--- directory/js/main.js
+++ directory/js/main.js
@@ -1,35 +1,22 @@
-const requestURL = 'https://chrmux.github.io/wdd230/directory/js/data.json';
+const requestURL = '/directory/js/data.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
+  .then(function (response) {return response.json();})
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    let directorys = jsonObject['directorys'];
-      directorys.forEach(displayBusiness);
+    console.table(jsonObject);
+    const directorys = jsonObject['directorys'];
+    directorys.forEach(displayCards);
+    directorys.forEach(displayListings);
   });
 
-function displayBusiness(directory) {
-  // Create elements to add to the document
+
+function displayCards(directorys) {
   let card = document.createElement('section');
-  let h3 = document.createElement('h3');
-  let portrait = document.createElement('img');
-  
+  let image = document.createElement('img');
 
+  image.src = directorys.imageurl;
 
-  // Change the textContent property of the h2 element to contain the prophet's full name
-  h3.textContent = directory.name;
-  
-  portrait.setAttribute('src', directory.url);
-  portrait.setAttribute('loading', 'lazy');
-  
-
-  // Add/append the section(card) with the h2 element
-  card.appendChild(h3);
-  card.appendChild(portrait);
-
-  // Add/append the existing HTML div with the cards class with the section(card)
+  card.appendChild(image);
   cards.appendChild(card);
 }
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,16 +1,9 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=ab85ba57bbbb423fb62bfb8201126ede"';
-
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=735d9a597ef22a51c2b271ac8cf9d39b"';
+document.querySelector('#current-temp').textContent = jsObject.main.temp;
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    
   });
-
-const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-const desc = jsObject.weather[0].description;
-document.querySelector('#current-temp').textContent = jsObject.main.temp;
-document.querySelector('#icon-src').textContent = iconsrc;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
