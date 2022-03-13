6315433b6573b8a00386ae639be634135bd07c7c
chrmux <jnmarollano@gmail.com>
2022-03-14 06:43:57 +0800

    Added

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -36,16 +36,16 @@ const date2 = document.querySelector("currentdate2");
 /* navbar */
 
 const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
 const items = document.querySelectorAll(".item");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
     toggle.querySelector("a").innerHTML = "";
   } else {
-    menu.classList.add("active");
+    navbar.classList.add("active");
     toggle.querySelector("a").innerHTML = "";
   }
 }
@@ -54,8 +54,8 @@ function toggleMenu() {
 function toggleItem() {
   if (this.classList.contains("submenu-active")) {
     this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  } else if (navbar.querySelector(".submenu-active")) {
+    navbar.querySelector(".submenu-active").classList.remove("submenu-active");
     this.classList.add("submenu-active");
   } else {
     this.classList.add("submenu-active");
@@ -64,10 +64,10 @@ function toggleItem() {
 
 /* Close Submenu From Anywhere */
 function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
+  let isClickInside = navbar.contains(e.target);
 
-  if (!isClickInside && menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  if (!isClickInside && navbar.querySelector(".submenu-active")) {
+    navbar.querySelector(".submenu-active").classList.remove("submenu-active");
   }
 }
 /* Event Listeners */
