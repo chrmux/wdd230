ca1da80e128f981c93a342a845067d1b801815c5
chrmux <jnmarollano@gmail.com>
2022-04-07 22:19:07 +0800

    Added

--- templeinn&suites/js/main.js
+++ templeinn&suites/js/main.js
@@ -1,7 +1,7 @@
 
 //-- contact --//
 
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
+document.querySelector('.contact-form').addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.elements.name.value = '';
   e.target.elements.email.value = '';
